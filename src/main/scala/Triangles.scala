17c73410235c84007123e9e3cab41519d8969065
philipschwarz <philip.johann.schwarz@gmail.com>
2022-03-12 14:11:10 +0000

    exploit Scala's strengths

--- src/main/scala/Triangles.scala
+++ src/main/scala/Triangles.scala
@@ -1,8 +1,24 @@
 import java.awt.{Color, Dimension, Graphics}
 import javax.swing.*
 
+case class Point(x: Float, y: Float)
+
+case class Triangle(a: Point, b: Point, c: Point)
+object Triangle:
+  def apply(centre: Point, side: Float, height: Float): Triangle =
+    val Point(x,y) = centre
+    val halfSide = 0.5F * side
+    val bottomLeft = Point(x - halfSide, y - 0.5F * height)
+    val bottomRight = Point(x + halfSide, y - 0.5F * height)
+    val top = Point(x, y + 0.5F * height )
+    Triangle(bottomLeft,bottomRight,top)
+
+extension (p: Point)
+  def deviceCoords(panelHeight: Int): (Int, Int) =
+    (Math.round(p.x), panelHeight - Math.round(p.y))
+
 @main def main: Unit =
-  // Create the frame on the event dispatching thread.
+// Create the panel on the event dispatching thread.
   SwingUtilities.invokeLater(
     new Runnable():
       def run: Unit = Triangles()
@@ -20,54 +36,34 @@ class TrianglesPanel extends JPanel:
 
   setBackground(Color.white)
 
-  var maxX, maxY, minMaxXY, xCenter, yCenter = 0
-
-  def initgr: Unit = {
-    val d: Dimension = getSize() 
-    maxX = d.width - 1 
-    maxY = d.height - 1 
-    minMaxXY = Math.min(maxX, maxY) 
-    xCenter = maxX / 2 
-    yCenter = maxY / 2 
-  }
-
-  def iX(x: Float) = Math.round(x)
-  def iY(y: Float) = maxY - Math.round(y)
-
   override def paintComponent(g: Graphics): Unit =
 
     super.paintComponent(g)
 
-    initgr
+    val panelSize: Dimension = getSize()
+    val panelWidth = panelSize.width - 1
+    val panelHeight = panelSize.height - 1
+    val panelCentre = Point(panelWidth / 2, panelHeight / 2)
+    val triangleSide: Float = 0.95F * Math.min(panelWidth, panelHeight)
+    val triangleHeight: Float = (0.5F * triangleSide) * Math.sqrt(3).toFloat
 
-    val side: Float = 0.95F * minMaxXY
-    val sideHalf: Float = 0.5F * side
-    val h: Float = sideHalf * Math.sqrt(3).toFloat
-    val q: Float = 0.05F 
-    val p: Float = 1 - q 
+    val shrinkAndTwist: Triangle => Triangle =
+      val q = 0.05F
+      val p = 1 - q
+      def combine(a: Point, b: Point) = Point(p * a.x + q * b.x, p * a.y + q * b.y)
+      { case Triangle(a,b,c) => Triangle(combine(a,b), combine(b,c), combine(c,a)) }
 
-    var xA, yA, xB, yB, xC, yC = 0F
-    var xA1, yA1, xB1, yB1, xC1, yC1 = 0F
+    def drawLine(a: Point, b: Point): Unit =
+      val (ax,ay) = a.deviceCoords(panelHeight)
+      val (bx,by) = b.deviceCoords(panelHeight)
+      g.drawLine(ax, ay, bx, by)
 
-    xA = xCenter - sideHalf 
-    yA = yCenter - 0.5F * h 
-    xB = xCenter + sideHalf 
-    yB = yA 
-    xC = xCenter 
-    yC = yCenter + 0.5F * h 
+    val draw: Triangle => Unit =
+      case Triangle(a, b, c) =>
+      drawLine(a, b)
+      drawLine(b, c)
+      drawLine(c, a)
 
-    for (i <- 1 to 50)
+    val triangle = Triangle(panelCentre, triangleSide, triangleHeight)
 
-      g.drawLine(iX(xA), iY(yA), iX(xB), iY(yB)) 
-      g.drawLine(iX(xB), iY(yB), iX(xC), iY(yC)) 
-      g.drawLine(iX(xC), iY(yC), iX(xA), iY(yA)) 
-
-      xA1 = p * xA + q * xB 
-      yA1 = p * yA + q * yB 
-      xB1 = p * xB + q * xC 
-      yB1 = p * yB + q * yC 
-      xC1 = p * xC + q * xA 
-      yC1 = p * yC + q * yA 
-
-      xA = xA1;  xB = xB1;  xC = xC1
-      yA = yA1;  yB = yB1;  yC = yC1
+    LazyList.iterate(triangle)(shrinkAndTwist).take(50).foreach(draw)
